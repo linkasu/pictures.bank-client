3493fa6003f14727438702a6b9b5fa269d638d8b
Ivan Bakaidov <ivan@aacidov.ru>
2023-12-07 20:55:25 +0000

    Refactor API calls to return response data

--- src/PicturesApi.ts
+++ src/PicturesApi.ts
@@ -6,23 +6,23 @@ export class PicturesApi {
     constructor(private axiosInstance: AxiosInstance) { }
 
     getAllPictures(): Promise<Picture[]> {
-        return this.axiosInstance.get("/picture/all");
+        return this.axiosInstance.get("/picture/all").then(response => response.data)  
     }
 
     getPicturesByCategory(categoryId: string): Promise<Picture[]> {
-        return this.axiosInstance.get(`/picture/category/${categoryId}`);
+        return this.axiosInstance.get(`/picture/category/${categoryId}`).then(response => response.data)
     }
 
     getCategoriesByPicture(id: string): Promise<Category[]> {
-        return this.axiosInstance.get(`/picture/${id}/categories`);
+        return this.axiosInstance.get(`/picture/${id}/categories`).then(response => response.data);
     }
 
     getPictureBufferById(id: string): Promise<Buffer> {
-        return this.axiosInstance.get(`/picture/${id}/buffer`, {responseType: 'arraybuffer'}).then(response => response.data);
+        return this.axiosInstance.get(`/picture/${id}/buffer`, {responseType: 'arraybuffer'}).then(response => response.data).then(response => response.data);
     }
 
     createPicture(data: CreatePictureInput): Promise<Picture> {
-        return this.axiosInstance.post("/picture/create", data);
+        return this.axiosInstance.post("/picture/create", data).then(response => response.data);
     }
 
     uploadPicture(id: string, file: Buffer): Promise<Picture> {
@@ -32,28 +32,28 @@ export class PicturesApi {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
-        });
+        }).then(response => response.data);
     }
 
     deletePicture(id: string): Promise<Picture> {
-        return this.axiosInstance.delete(`/picture/${id}`);
+        return this.axiosInstance.delete(`/picture/${id}`).then(response => response.data);
     }
 
     addCategory(pictureId: string, categoryId: string): Promise<void> {
-        return this.axiosInstance.post(`/picture/${pictureId}/linkToCategory`, { categoryId });
+        return this.axiosInstance.post(`/picture/${pictureId}/linkToCategory`, { categoryId }).then(response => response.data);
     }
 
     removeCategory(pictureId: string, categoryId: string): Promise<void> {
-        return this.axiosInstance.post(`/picture/${pictureId}/unlinkFromCategory`, { categoryId });
+        return this.axiosInstance.post(`/picture/${pictureId}/unlinkFromCategory`, { categoryId }).then(response => response.data);
     }
     renamePicture(id: string, name: string): Promise<Picture> {
-        return this.axiosInstance.post(`/picture/${id}/rename`, { name });
+        return this.axiosInstance.post(`/picture/${id}/rename`, { name }).then(response => response.data);
     }
     searchPictures(query: string): Promise<Picture[]> {
         return this.axiosInstance.get(`/picture/search/`, {
             params: {
                 query
             }
-        });
+        }).then(response => response.data).then(response => response.data);
     }
 }
